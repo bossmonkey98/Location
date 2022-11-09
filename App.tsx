d805b8684d29996a1feb409e005a532f9be8a89e
bossmonkey98 <arunambregol007@gmail.com>
2022-11-09 12:34:53 +0530

    Screen routes added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { LocationProvider } from './context/LocationContext';
+import { MapDetailsProvider } from './context/MapDetailsContext';
+import Home from './Screens/Home';
+import Map from './Screens/Map';
+
+export type ScreenParamTypes ={
+  'Home':undefined,
+  "Map":undefined
+}
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator<ScreenParamTypes>()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapDetailsProvider>
+      <LocationProvider>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={Home} options={{title:'Location Manager'}}/>
+            <Stack.Screen name='Map' component={Map} options={{title:'Your Location'}}/>
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+        </View>
+      </NavigationContainer>
+      </LocationProvider>
+    </MapDetailsProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
